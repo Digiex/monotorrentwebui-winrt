802cdb8ca14d7b5e6a00f6511361bdc1ca9391b1
SND\sergueim_cp <SND\sergueim_cp@a83551a4-30f6-4d81-a974-c6ced450ddbf>
2008-12-21 08:22:37 +0000

    Updated assembly info
    git-svn-id: https://monotorrentwebui.svn.codeplex.com/svn@265 a83551a4-30f6-4d81-a974-c6ced450ddbf

--- MonoTorrent.WebUI/MonoTorrent.ClientService/Properties/AssemblyInfo.cs
+++ MonoTorrent.WebUI/MonoTorrent.ClientService/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MonoTorrent.Service")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("MonoTorrent.ClientService")]
+[assembly: AssemblyDescription("MonoTorrent client with µTorrent's WebUI")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("MonoTorrent.Service")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2008")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MonoTorrent WebUI")]
+[assembly: AssemblyCopyright("Serguei Michtchenko; Ms-RL")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.*")]
